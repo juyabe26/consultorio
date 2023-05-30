3d5d46f87eee7afeee8e6aee374f4b98121675eb
PERFICIENT\julian.buitronr <julian.buitronr@perficient.com>
2023-05-30 15:36:11 -0500

    Inicio del proyecto

---
+++ src/test/java/com/perficient/consultorio/ConsultorioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perficient.consultorio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultorioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
