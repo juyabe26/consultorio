b97a61b15b756419ac95bee1f7d2a22140a07a62
PERFICIENT\julian.buitronr <julian.buitronr@perficient.com>
2023-07-19 14:41:51 -0500

    integracion keyCloak

--- src/main/java/com/perficient/consultorio/ConsultorioApplication.java
+++ src/main/java/com/perficient/consultorio/ConsultorioApplication.java
@@ -2,6 +2,9 @@ package com.perficient.consultorio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
 @SpringBootApplication
 public class ConsultorioApplication {
 
@@ -9,5 +12,8 @@ public class ConsultorioApplication {
 		SpringApplication.run(ConsultorioApplication.class, args);
 	}
 
-
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
 }
